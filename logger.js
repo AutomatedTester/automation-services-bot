de387758f85f396004c7415ddb71e54b510c5ef7
AutomatedTester <dburns@mozilla.com>
2012-02-15 09:45:09 +0000

    whitespace cleanup

--- logger.js
+++ logger.js
@@ -20,37 +20,36 @@ mongoose.model('message', Message);
 var Message = mongoose.model('message');
 
 exports.log = function(messageObject, callback){
-      mongoose.connect('mongodb://localhost/botmessages');
-        var redisClient = redis.createClient();
-        redisClient.on("error", function (err) {
-            console.log("Redis connection error to " + redisClient.host + ":" + redisClient.port + " - " + err);
-        });
-        var now = Date.now()
-        messageObject["timestamp"] = now;
-        messageObject.created_at = now;
-        redisClient.rpush('automation', JSON.stringify(messageObject));
-        var msg = new Message({
-                    channel: messageObject.channel,
-                    from: messageObject.from,
-                    message:messageObject.message,
-                    action: messageObject.action,
-                    who: messageObject.who,
-                    reason: messageObject.reason,
-                    oldnick: messageObject.oldnick,
-                    newnick: messageObject.newnick,
-                    created_at: messageObject.created_at,
-                    });
-        msg.save(function(err){
-          if (err) {
-            console.error(err);
-            throw err;
-            }
-        });
-        redisClient.publish("automation", JSON.stringify(messageObject));
-        redisClient.quit();
-        mongoose.disconnect();
-        callback();
-
+  mongoose.connect('mongodb://localhost/botmessages');
+  var redisClient = redis.createClient();
+  redisClient.on("error", function (err) {
+    console.log("Redis connection error to " + redisClient.host + ":" + redisClient.port + " - " + err);
+  });
+  var now = Date.now()
+  messageObject["timestamp"] = now;
+  messageObject.created_at = now;
+  redisClient.rpush('automation', JSON.stringify(messageObject));
+  var msg = new Message({
+    channel: messageObject.channel,
+    from: messageObject.from,
+    message:messageObject.message,
+    action: messageObject.action,
+    who: messageObject.who,
+    reason: messageObject.reason,
+    oldnick: messageObject.oldnick,
+    newnick: messageObject.newnick,
+    created_at: messageObject.created_at,
+  });
+  msg.save(function(err){
+    if (err) {
+      console.error(err);
+      throw err;
+    }
+  });
+  redisClient.publish("automation", JSON.stringify(messageObject));
+  redisClient.quit();
+  mongoose.disconnect();
+  callback();
 }
 
 exports.Message = Message;
