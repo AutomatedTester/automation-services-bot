ae95a5c6ee592920759b2860777183c9aba3f067
Dave Hunt <dave.hunt@gmail.com>
2012-03-30 17:17:47 +0100

    Fixed issue with project name in :issues response.

--- bot.js
+++ bot.js
@@ -219,8 +219,8 @@ client.addListener('message', function (from, to, message) {
     if (project !== null){
       var key = to.substring(1).toLowerCase();
       console.log(key);
-      if (github[key][project[1]]){
-        client.say(to, "Issues for " + project +  " can be found at " + github[key][project[1]] + "/issues");
+      if (github[key] && github[key][project[1]]){
+        client.say(to, "Issues for " + project[1] +  " can be found at " + github[key][project[1]] + "/issues");
       } else {
         client.say(to, "I am sorry I don't know of that project. Please raise an issue on " +
             "http://oss.theautomatedtester.co.uk/automation-services-bot/ if I should know about it");
